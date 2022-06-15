0c74f1e4b17b31a2cfc2f023f7cd44b15a9129a9
renedsoft <renedsoft@gmail.com>
2022-06-14 22:16:07 -0400

    user image

--- src/app/auth/new/register/register.component.ts
+++ src/app/auth/new/register/register.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { ApiHelperService, NotificationService, RequestService, ValidationsService } from '@core/services';
+import {
+  ApiHelperService,
+  NotificationService,
+  RequestService,
+  ValidationsService,
+} from '@core/services';
 import { Router } from '@angular/router';
 import { HttpEventType } from '@angular/common/http';
 import * as moment from 'moment';
 
-
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-
   public showPassword = false;
   public registerForm = this.formBuilder.group({
     first_name: [null, [Validators.required]],
@@ -33,7 +38,7 @@ export class RegisterComponent implements OnInit {
     geschaftsfuhrer: [null, [Validators.required]],
     iban: [null, [Validators.required]],
     bic_swift_code: [null, [Validators.required]],
-    hrb_walsrode: [null, [Validators.required]]
+    hrb_walsrode: [null, [Validators.required]],
   });
   public logoImg: File | undefined;
   public showLogo: boolean = false;
@@ -46,12 +51,11 @@ export class RegisterComponent implements OnInit {
     private readonly apiHelperService: ApiHelperService,
     private readonly router: Router,
     private readonly validationsService: ValidationsService,
-    private readonly notificationService: NotificationService
-  ) {
-  }
+    private readonly notificationService: NotificationService,
+    private httpClient: HttpClient
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   public hasError = (input: string): boolean => {
     return this.validationsService.hasError(this.registerForm, input);
@@ -71,46 +75,139 @@ export class RegisterComponent implements OnInit {
 
   togglePwd = () => (this.showPassword = !this.showPassword);
 
+  public POSTUpload2 = (url: string, files: FormData): Observable<any> => {
+    let cabcera = new HttpHeaders({ Accept: 'application/json' });
+    return this.httpClient.post(url, files, {
+      headers: cabcera,
+      reportProgress: true,
+      observe: 'events',
+    });
+  };
+
   public register() {
-    if (this.registerForm.valid &&  !this.isLoading) {
-    // if (this.registerForm.valid && this.logoImg instanceof File && !this.isLoading) {
+    if (
+      this.registerForm.valid &&
+      this.logoImg instanceof File &&
+      !this.isLoading
+    ) {
       this.isLoading = true;
-      const newUser = { ...this.registerForm.value, active_until: moment().add(30, 'days').format('YYYY-MM-DD') }
+      const newUser = {
+        ...this.registerForm.value,
+        active_until: moment().add(30, 'days').format('YYYY-MM-DD'),
+      };
+
+      const form = new FormData();
+      form.append('files', <File>this.logoImg);
+
+      this.POSTUpload2(this.apiHelperService.uploadFilesURL, form).subscribe(
+        (events) => {
+          if (events.type === HttpEventType.Response) {
+            // const data = { logo: events.body[0].id, user: res.user.id };
+            const data = { logo: events.body[0].id };
+
+            this.requestService
+              .Post(this.apiHelperService.logosURL, data)
+              .subscribe((respuesta) => {
+                newUser.logo = respuesta.data.id;
+                this.requestService
+                  .Post(this.apiHelperService.registerURL, newUser, false)
+                  .subscribe((res) => {
+                    this.isLoading = false;
+                    this.notificationService.riseNotification({
+                      color: 'success',
+                      data: 'Registrierung & Anmeldung erfolgreich',
+                    });
+                  });
+
+                // this.router.navigate(['/admin']);
+              });
+          }
+        }
+      );
+    }
+  }
+
+  public register2() {
+    if (
+      this.registerForm.valid &&
+      this.logoImg instanceof File &&
+      !this.isLoading &&
+      this.registerForm.valid && this.logoImg instanceof File && !this.isLoading
+    ) {
+      this.isLoading = true;
+      const newUser = {
+        ...this.registerForm.value,
+        active_until: moment().add(30, 'days').format('YYYY-MM-DD'),
+      };
       this.requestService
         .Post(this.apiHelperService.registerURL, newUser, false)
         .subscribe((res) => {
           const form = new FormData();
           form.append('files', <File>this.logoImg);
 
-
-          this.isLoading = false;
+          this.POSTUpload2(
+            this.apiHelperService.uploadFilesURL,
+            form
+          ).subscribe((events) => {
+            if (events.type === HttpEventType.Response) {
+              const data = { logo: events.body[0].id, user: res.user.id };
+              this.requestService
+                .Post(this.apiHelperService.logosURL, data)
+                .subscribe(() => {
+                  this.isLoading = false;
                   this.notificationService.riseNotification({
                     color: 'success',
-                    data: 'Registrierung & Anmeldung erfolgreich'
+                    data: 'Registrierung & Anmeldung erfolgreich',
                   });
                   // this.router.navigate(['/admin']);
-          //TODO: Enviar imagen al servidor
+                });
+            }
+          });
+        });
+    }
+  }
 
-          // this.requestService.POSTUpload(this.apiHelperService.uploadFilesURL, form).subscribe((events) => {
-          //   if (events.type === HttpEventType.Response) {
-          //     const data = { logo: events.body[0].id, user: res.user.id };
-          //     this.requestService.Post(this.apiHelperService.logosURL, data)
-          //       .subscribe(() => {
-          //         this.isLoading = false;
-          //         this.notificationService.riseNotification({
-          //           color: 'success',
-          //           data: 'Registrierung & Anmeldung erfolgreich'
-          //         });
-          //         // this.router.navigate(['/admin']);
-          //       });
-          //   }
-          // });
+  public registerOLD() {
+    if (this.registerForm.valid && !this.isLoading) {
+      // if (this.registerForm.valid && this.logoImg instanceof File && !this.isLoading) {
+      this.isLoading = true;
+      const newUser = {
+        ...this.registerForm.value,
+        active_until: moment().add(30, 'days').format('YYYY-MM-DD'),
+      };
+
+      this.requestService
+        .Post(this.apiHelperService.registerURL, newUser, false)
+        .subscribe((res) => {
+          const form = new FormData();
+
+          form.append('files', <File>this.logoImg);
+
+          console.log(this.logoImg);
+
+          this.requestService
+            .POSTUpload(this.apiHelperService.uploadFilesURL, form)
+            .subscribe((events) => {
+              if (events.type === HttpEventType.Response) {
+                const data = { logo: events.body[0].id, user: res.user.id };
+
+                this.requestService
+                  .Post(this.apiHelperService.logosURL, data)
+                  .subscribe(() => {
+                    this.isLoading = false;
+                    this.notificationService.riseNotification({
+                      color: 'success',
+                      data: 'Registrierung & Anmeldung erfolgreich',
+                    });
+                  });
+              }
+            });
         });
     } else {
       this.isLoading = false;
       this.notificationService.riseNotification({
         color: 'error',
-        data: 'Data Errors!!!'
+        data: 'Data Errors!!!',
       });
     }
   }
@@ -121,5 +218,4 @@ export class RegisterComponent implements OnInit {
     this.logoImgSrc = window.URL.createObjectURL(file);
     this.showLogo = true;
   }
-
 }
