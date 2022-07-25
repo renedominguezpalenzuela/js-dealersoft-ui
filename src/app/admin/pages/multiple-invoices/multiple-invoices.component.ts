c1c3b3562431818c89fb8e99e7901d070d933b75
renedsoft <renedsoft@gmail.com>
2022-07-25 11:38:47 -0400

    cancel invoice

--- src/app/admin/pages/multiple-invoices/multiple-invoices.component.ts
+++ src/app/admin/pages/multiple-invoices/multiple-invoices.component.ts
@@ -1,7 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Invoice } from '@core/interfaces';
-import { Column, ColumnType, OperationEvent, OptionSettings } from '@core/lib/dynamic-table/utils/interfaces';
-import { ApiHelperService, LoadingService, RequestService , AuthService} from '@core/services';
+import {
+  Column,
+  ColumnType,
+  OperationEvent,
+  OptionSettings,
+} from '@core/lib/dynamic-table/utils/interfaces';
+import {
+  ApiHelperService,
+  LoadingService,
+  RequestService,
+  NotificationService,
+  AuthService,
+} from '@core/services';
 import { delay } from 'rxjs';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { ExportType } from '@core/services/request.service';
@@ -10,17 +21,19 @@ import * as moment from 'moment';
 import { MatDialog } from '@angular/material/dialog';
 import { InvoiceInfoComponent } from '@core/components/invoice-info/invoice-info.component';
 
-import {  FilterDeepOption } from '@core/interfaces';
+import { FilterDeepOption } from '@core/interfaces';
 import { FilterOperator } from '@core/interfaces/query-params';
+
+import { CreateInvoiceService } from '../../../servicios/create-invoice.service';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-multiple-invoices',
   templateUrl: './multiple-invoices.component.html',
-  styleUrls: ['./multiple-invoices.component.scss']
+  styleUrls: ['./multiple-invoices.component.scss'],
 })
 export class MultipleInvoicesComponent implements OnInit {
-
-
   private currentUserId: number | undefined;
 
   public data: Invoice[] = [];
@@ -30,7 +43,7 @@ export class MultipleInvoicesComponent implements OnInit {
       header: 'Number',
       show: true,
       type: ColumnType.Regular,
-      ordenar: true
+      ordenar: true,
     },
 
     {
@@ -38,7 +51,7 @@ export class MultipleInvoicesComponent implements OnInit {
       header: 'Ref Number',
       show: true,
       type: ColumnType.Regular,
-      ordenar: true
+      ordenar: true,
     },
 
     {
@@ -46,70 +59,75 @@ export class MultipleInvoicesComponent implements OnInit {
       header: 'Cancel Num',
       show: true,
       type: ColumnType.Regular,
-      ordenar: true
+      ordenar: true,
     },
-
 
     {
       column: 'attributes.title',
       header: 'Titel',
       show: true,
       type: ColumnType.Regular,
-      ordenar: false
+      ordenar: false,
     },
     {
       column: 'attributes.date',
       header: 'Datum',
       show: true,
       type: ColumnType.Date,
-      ordenar: false
+      ordenar: false,
     },
     {
       column: 'attributes.delivery_date',
       header: 'Lieferdatum',
       show: true,
       type: ColumnType.Date,
-      ordenar: false
+      ordenar: false,
     },
     {
       column: 'attributes.a25',
       header: '§25a',
       show: true,
       type: ColumnType.Boolean,
-      ordenar: false
+      ordenar: false,
     },
     {
       column: 'attributes.iva',
       header: 'inkl. MwSt.',
       show: true,
       type: ColumnType.Boolean,
-      ordenar: false
-    }
+      ordenar: false,
+    },
   ];
-
 
   public OptionSettings: OptionSettings = {
     options: [
       {
-        icon: 'picture_as_pdf',
-        literal: 'PDF exportieren',
-        event: 'ExportPdf'
+        icon: 'edit',
+        literal: 'Detail anzeigen',
+        event: 'Detail',
       },
       {
-        icon: 'info',
-        literal: 'Detail anzeigen',
-        event: 'Detail'
-      }
-    ]
+        icon: 'picture_as_pdf',
+        literal: 'PDF exportieren',
+        event: 'ExportPdf',
+      },
+      {
+        icon: 'delete',
+        literal: 'Storno Rechnung erstellen',
+        event: 'Cancel',
+      },
+    ],
   };
-
 
   public filterQuery: string = '';
   public loading: boolean = false;
   public noShowLoader = true;
   public pageCount: number = 100;
   public currentPage: number = 1;
-  public extraOption: { event: string, icon: string } = { event: 'Detail', icon: 'chevron_right' };
+  public extraOption: { event: string; icon: string } = {
+    event: 'Detail',
+    icon: 'chevron_right',
+  };
 
   constructor(
     private readonly apiHelperService: ApiHelperService,
@@ -118,33 +136,38 @@ export class MultipleInvoicesComponent implements OnInit {
     private readonly router: Router,
     private readonly activatedRoute: ActivatedRoute,
     private readonly matDialog: MatDialog,
-    private readonly authService: AuthService
+    private readonly authService: AuthService,
+    private readonly createInvoice: CreateInvoiceService,
+    private readonly notificationService: NotificationService,
   ) {
     if (this.activatedRoute.snapshot.queryParamMap.has('page')) {
-      this.currentPage = <number><unknown>this.activatedRoute.snapshot.queryParamMap.get('page');
+      this.currentPage = <number>(
+        (<unknown>this.activatedRoute.snapshot.queryParamMap.get('page'))
+      );
     }
     this.noShowLoader = false;
-   
   }
 
   ngOnInit(): void {
-
     this.authService.currentUser.subscribe((user) => {
-      this.currentUserId = user?.id
+      this.currentUserId = user?.id;
       this.loadPaginatedData(this.currentPage);
-    })
-    this.loadingService.loadingSub.pipe(delay(0)).subscribe((loading) => this.loading = loading);
+    });
+    this.loadingService.loadingSub
+      .pipe(delay(0))
+      .subscribe((loading) => (this.loading = loading));
 
     this.activatedRoute.queryParamMap.subscribe((map: ParamMap) => {
       if (map.has('page'))
-        this.currentPage = <number><unknown>map.get('page');
+        this.currentPage = <number>(<unknown>map.get('page'));
     });
   }
 
   public loadPaginatedData = ($event: number) => {
     this.currentPage = $event;
-    this.requestService.Get(this.apiHelperService.invoicesURL, this.query(this.currentUserId))
-      .subscribe(res => {
+    this.requestService
+      .Get(this.apiHelperService.invoicesURL, this.query(this.currentUserId))
+      .subscribe((res) => {
         this.data = res.data;
         this.pageCount = res.meta.pagination.pageCount;
         this.router.navigate([], {
@@ -154,11 +177,12 @@ export class MultipleInvoicesComponent implements OnInit {
         });
         this.noShowLoader = true;
       });
-  }
+  };
 
   public catchEvent($event: OperationEvent) {
     switch ($event.type) {
       case 'ExportPdf':
+        this.generatePdf($event.value.id, $event.value.attributes);
         // this.requestService.downloadPDF(this.apiHelperService.pdfURL, {
         //   type: ExportType.invoice,
         //   invoiceId: $event.value.id
@@ -184,14 +208,44 @@ export class MultipleInvoicesComponent implements OnInit {
         // this.router.navigate(['/admin/new-invoice', $event.value.id] );
         // this.router.navigate(['/admin/new-invoice/${event.value.id}'] );
 
-        this.router.navigate(['/admin/new-invoice/', $event.value.id])
+        this.router.navigate(['/admin/new-invoice/', $event.value.id]);
+        break;
+
+      case 'Cancel':
+        this.cancelInvoice($event.value.id, $event.value.attributes);
         break;
       default:
         break;
     }
   }
 
+  public generatePdf = (id: any, invoice_data: any) => {
+    // this.Prueba();
 
+    let tipo = '/';
+    if (invoice_data.a25) {
+      tipo = 'reports/bill/a25';
+    } else {
+      tipo = 'reports/bill/iva';
+    }
+
+    this.requestService
+      .downloadPDF(this.apiHelperService.pdfURL, {
+        // type: ExportType.vehicle,
+        type: tipo,
+        id: id, //no es un car
+      })
+      .subscribe((res) => {
+        let nombre = `Storno Rechnung St.-Nr. ${invoice_data?.invoice_number} für Re Nr. ${invoice_data?.reference_invoice_number} .pdf`;
+        if (invoice_data?.invoice_type === 1) {
+          nombre = `Rechnung_${invoice_data?.invoice_number}_(${moment().format(
+            'YYYY-MM-DD'
+          )}).pdf`;
+        }
+
+        saveAs(new Blob([res], { type: 'application/pdf' }), nombre);
+      });
+  };
 
   /**
    * 
@@ -210,22 +264,95 @@ export class MultipleInvoicesComponent implements OnInit {
     });
    */
 
-  private query = (id: any) => this.requestService.generateQuery({
-    populate: ['*'],
-    filters: [
-      {
-        field: '[owner][id]',
-        value: id,
-        operator: FilterOperator.$eq,
-         option: FilterDeepOption.$and,
-      },
-    ],
-    'pagination[pageSize]': 10,
-    'pagination[page]': this.currentPage
-  });
+  private query = (id: any) =>
+    this.requestService.generateQuery({
+      populate: ['*'],
+      filters: [
+        {
+          field: '[owner][id]',
+          value: id,
+          operator: FilterOperator.$eq,
+          option: FilterDeepOption.$and,
+        },
+      ],
+      'pagination[pageSize]': 10,
+      'pagination[page]': this.currentPage,
+    });
 
-  private queryInvoice = () => this.requestService.generateQuery({
-    populate: ['client']
-  });
+  private queryInvoice = () =>
+    this.requestService.generateQuery({
+      populate: ['client'],
+    });
 
+
+
+    
+  public generateCancelInvoice_Number(): Observable<any> {
+    return this.createInvoice.generateInvoice_Number();
+  }
+
+  cancelInvoice(id: any, invoice_data: any) {
+    //si el tipo de invoice a cancelar es 2, no se puede cancelar
+    //Eine Stornorechnung kann nicht storniert werden
+    //si el invoice a cancelar esta ya cancelada no se puede cancelar
+    //Eine stornierte Rechnung kann nicht storniert werden
+
+    if (invoice_data.cancelled) {
+      this.notificationService.riseNotification({
+        color: 'warning',
+        data: 'Eine stornierte Rechnung kann nicht storniert werden',
+      });
+
+      return;
+    }
+
+    if (invoice_data.invoice_type === 2) {
+      this.notificationService.riseNotification({
+        color: 'warning',
+        data: 'Eine Stornorechnung kann nicht storniert werden',
+      });
+      return;
+    }
+
+    this.generateCancelInvoice_Number().subscribe((numero_invoice: any) => {
+      this.createInvoice
+        .generateCancelInvoice_Number()
+        .subscribe((numero_cancelacion: any) => {
+          let datosInvoice = {
+            ...invoice_data,
+            invoice_number: numero_invoice,
+            cancel_number: numero_cancelacion,
+            reference_invoice_number: invoice_data.invoice_number,
+            cancelled: false,
+            invoice_type: 2,
+            client: invoice_data?.client.data.id,
+            owner: invoice_data?.owner.data.id,
+            title:
+              'Rechnungsnummer ' + invoice_data.invoice_number + ' stornieren',
+            // +this.invoice_data.title
+          };
+
+          this.createInvoice
+            .guardarInvoiceFromSellCar(datosInvoice)
+            .subscribe(() => {
+              //Creada nueva invoice
+
+              this.requestService
+                .Put(this.apiHelperService.invoicesURL + '/' + id, {
+                  cancelled: true,
+                })
+                .subscribe(() => {
+                  this.notificationService.riseNotification({
+                    color: 'success',
+                    data: 'Stornorechnung erstellt',
+                  });
+                  window.location.reload()
+
+
+//                  this.router.navigate(['/admin/list-invoices']);
+                });
+            });
+        });
+    });
+  }
 }
--- src/app/admin/pages/new-bill/new-bill.component.ts
+++ src/app/admin/pages/new-bill/new-bill.component.ts
@@ -40,11 +40,13 @@ import {
   MomentDateAdapter,
   MAT_MOMENT_DATE_ADAPTER_OPTIONS,
 } from '@angular/material-moment-adapter';
+
 import {
   DateAdapter,
   MAT_DATE_FORMATS,
   MAT_DATE_LOCALE,
 } from '@angular/material/core';
+
 import 'moment/locale/de';
 
 @Component({
