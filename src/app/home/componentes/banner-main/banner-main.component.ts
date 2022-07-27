f53c15be40cca58b071938f5e0b67bf8f1be2b6a
renedsoft <renedsoft@gmail.com>
2022-07-27 17:05:47 -0400

    new landing page

---
+++ src/app/home/componentes/banner-main/banner-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-main',
+  templateUrl: './banner-main.component.html',
+  styleUrls: ['./banner-main.component.scss']
+})
+export class BannerMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
