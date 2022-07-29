a6c32a54e3d486291a7c329cb8599180992a2e45
ruben97w33 <w33cuba@gmail.com>
2022-07-29 01:58:44 -0500

    some changes in register

--- src/app/home/componentes/acordion/acordion.component.ts
+++ src/app/home/componentes/acordion/acordion.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AcordionComponent implements OnInit {
   public panelOpenState = false;
+  public panelOpenState2 = false;
+  public panelOpenState3 = false;
+  public panelOpenState4 = false;
+  public panelOpenState5 = false;
   constructor() {}
 
   ngOnInit(): void {}
