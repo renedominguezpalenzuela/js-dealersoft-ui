df0764981831c82ab4e285552988fe17952d935b
ruben97w33 <w33cuba@gmail.com>
2022-07-29 01:39:46 -0500

    add animation-scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  ngOnInit(): void {
+    AOS.init();
+  }
 }
