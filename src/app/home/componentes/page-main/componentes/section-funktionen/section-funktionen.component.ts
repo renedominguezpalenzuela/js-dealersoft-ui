afdfda4950a7a98ddd6ac1d47869001b419b7ac7
renedsoft <renedsoft@gmail.com>
2022-08-03 16:29:41 -0400

    text

--- src/app/home/componentes/page-main/componentes/section-funktionen/section-funktionen.component.ts
+++ src/app/home/componentes/page-main/componentes/section-funktionen/section-funktionen.component.ts
@@ -8,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class SectionFunktionenComponent implements OnInit {
   public cards: any = [
     {
-      title: 'Fahrzeuge detailliert ins System einpflegen',
+      title: 'Fahrzeuge detailiert ins System einpflegen',
       text: [
-        'Vom Anlegen eines neuen Fahrzeugprofils bis zum Abschluss des Kaufvertrags: Dealersoft begleitet Sie im gesamten Verkaufsprozess.',
+        //'Vom Anlegen eines neuen Fahrzeugprofils bis zum Abschluss des Kaufvertrags: Dealersoft begleitet Sie im gesamten Verkaufsprozess.',
+        'Perfekt auf Thren Autohandel abgestimmt: Pfiegen Sie alle relevanten Daten des Fahrzeuges in die Datenbank ein. Schnell&Unkompliziert von überall'
       ],
       path: '1.png',
       pathmovil: '1movil.png',
@@ -19,9 +20,11 @@ export class SectionFunktionenComponent implements OnInit {
       nameLink: 'Jetzt registrieren',
     },
     {
-      title: 'Kund:innen-Karteien anlegen und mit Informationen verknüpfen',
+      //title: 'Kund:innen-Karteien anlegen und mit Informationen verknüpfen',
+      title: 'Aktuelle Bestandsübersicht und in Archiv mit allen bisher verkauften Fahrzeugen',
       text: [
-        'Vom Anlegen eines neuen Fahrzeugprofils bis zum Abschluss des Kaufvertrags: Dealersoft begleitet Sie im gesamten Verkaufsprozess.',
+       // 'Vom Anlegen eines neuen Fahrzeugprofils bis zum Abschluss des Kaufvertrags: Dealersoft begleitet Sie im gesamten Verkaufsprozess.',
+       'Behalten Sie den überblick über Ihren aktuellen Bestand und speichern verkaufte Fahrzeuge sicher in Ihrer eigenen Fahrzeug-Datenbank. Informieren Sie sich auf einen Blick über Ihren Bestandswert oder Standtage. Drucken Sie sich gezielt Bestandslisten beliebiger Monte aus - mit einem Klick'
       ],
       path: '2.png',
       pathmovil: '2movil.png',
@@ -32,9 +35,11 @@ export class SectionFunktionenComponent implements OnInit {
     },
     {
       title:
-        'Aktuelle Bestandsübersicht und ein Archiv mit allen bisher verkauften Fahrzeugen ',
+        //'Aktuelle Bestandsübersicht und ein Archiv mit allen bisher verkauften Fahrzeugen ',
+        'Kund:innen-Daten anlegen und mit Informationen verknüpfen',
       text: [
-        'Vom Anlegen eines neuen Fahrzeugprofils bis zum Abschluss des Kaufvertrags: Dealersoft begleitet Sie im gesamten Verkaufsprozess.',
+        //'Vom Anlegen eines neuen Fahrzeugprofils bis zum Abschluss des Kaufvertrags: Dealersoft begleitet Sie im gesamten Verkaufsprozess.',
+        'Pflegen Sie Ihre Kundendaten gut strukturiert in Ihre Datenbank ein und ordnen Sie diese dem An- oder Verkauf Ihrer Fahrzeuge zu.'
       ],
       path: '3.png',
       pathmovil: '3movil.png',
@@ -45,10 +50,12 @@ export class SectionFunktionenComponent implements OnInit {
     },
     {
       title:
-        'Kaufverträge / Stornoverträge erstellen und diese bequem mit Fahrzeugen und Kund:innen verknüpfen',
+       // 'Kaufverträge / Stornoverträge erstellen und diese bequem mit Fahrzeugen und Kund:innen verknüpfen',
+       'Kaufverträge für An- und Verkauf, Rechnungen und Stornorechnungen mit einem Klick erstellen',
       text: [
-        'von Überall ob mit PC oder Handy in',
-        '- Sekunden PDF Verträge und Rechnungen erstellen.',
+       // 'von Überall ob mit PC oder Handy in',
+       // '- Sekunden PDF Verträge und Rechnungen erstellen.',
+       'Egal ob per App oder im Browser: Dealersoft ermöglicht Ihnen die Erstellung von Kaufverträgen und Rechnungen per Knopfdruck. Und das immer im gleichen Format, inklusive Firmenlogo blitzschnell per PDF aufs Handy oder den PC.'
       ],
       path: '4.png',
       pathmovil: '4movil.png',
@@ -59,28 +66,33 @@ export class SectionFunktionenComponent implements OnInit {
     },
     {
       title:
-        'Fahrzeug- und Kund:innenstatistiken einsehen und den Gewinn und Verlust ihres Autohandels im Überblick behalten',
+        //'Fahrzeug- und Kund:innenstatistiken einsehen und den Gewinn und Verlust ihres Autohandels im Überblick behalten',
+        'Rechnungen erstellen und verwalten',
       text: [
-        'Vom Anlegen eines neuen Fahrzeugprofils bis zum Abschluss des Kaufvertrags: Dealersoft begleitet Sie im gesamten Verkaufsprozess.',
+        // 'Vom Anlegen eines neuen Fahrzeugprofils bis zum Abschluss des Kaufvertrags: Dealersoft begleitet Sie im gesamten Verkaufsprozess.',
+        'Generieren Sie mit einem Klick eine fortlaufende Rechnungsnummer und erstellen Sie Ihre PDF   Rechnung. Egal ob Netto Export, $25A oder inkl mwst, Dealersoft ist Ihr Tool für einen schnellen,  einfachen Verkaufsprozess.'
+        
       ],
       path: '5.png',
       pathmovil: '5movil.png',
 
       link: '/',
       position: false,
-      nameLink: 'Learn more about encryption',
+      nameLink: 'Jetzt registrieren',
     },
     {
-      title: 'Rechnungen erstellen und verwalten',
+     // title: 'Rechnungen erstellen und verwalten',
+     title:'Fahrzeug und Kund innen Statistiken einsehen und den Gewinn und Verlust Ihres Autohandels im Überblick behalten',
       text: [
-        'Vom Anlegen eines neuen Fahrzeugprofils bis zum Abschluss des Kaufvertrags: Dealersoft begleitet Sie im gesamten Verkaufsprozess.',
+        //'Vom Anlegen eines neuen Fahrzeugprofils bis zum Abschluss des Kaufvertrags: Dealersoft begleitet Sie im gesamten Verkaufsprozess.',
+        ''
       ],
       path: '6.png',
       pathmovil: '6movil.png',
 
       link: '/',
       position: true,
-      nameLink: 'Learn more about encryption',
+      nameLink: 'Jetzt registrieren',
     },
   ];
   constructor() {}
