ceb94bcefde38a91bbe2179629d8565987e4101d
ruben97w33 <w33cuba@gmail.com>
2022-08-05 14:23:53 -0500

    add kontakt page and fixing main images funktionen

--- src/app/home/componentes/page-main/contact/contact.component.ts
+++ src/app/home/componentes/page-main/contact/contact.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  public selectedTab: any = 1;
+
+  ABG:boolean = false;
+
+  toggleABG(){
+    this.ABG = !this.ABG;
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public navigationTop() {
+    
+    window.scrollTo(0, 0);
+   
+  }
+  receiveMessage(msg: any) {
+    this.selectedTab = msg;
+
+    //alert(msg);
+  }
 }
